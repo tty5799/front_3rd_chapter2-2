0dda980f277e3a272e9c910c33110ea1f6585b30
윤성용 <iop017779@gmail.com>
2024-10-20 20:57:11 +0900

    refactor : useCoupons 훅 addCoupon 함수 추가

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,9 @@ import { useState } from 'react';
 import { Coupon } from '../../types.ts';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+
+  return { coupons, addCoupon };
 };
