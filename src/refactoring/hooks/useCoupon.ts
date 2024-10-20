237a06cfc095ae2526eefce47b31fac03409d4e0
윤성용 <iop017779@gmail.com>
2024-10-20 19:59:46 +0900

    refactor : useProducts훅 update add 함수  추가

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,5 +1,5 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { useState } from 'react';
+import { Coupon } from '../../types.ts';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
   return { coupons: [], addCoupon: () => undefined };
