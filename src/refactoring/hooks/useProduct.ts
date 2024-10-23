3acd367bbe4a2844ac453d89b3572fb4efc9de95
윤성용 <iop017779@gmail.com>
2024-10-24 01:46:17 +0900

    refactor : useProducts hook updateProduct 계산 함수로 분리

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import { Product } from '../../types.ts';
+import { updateProductInList } from './utils/productUtils.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
   const updateProduct = (updatedProduct: Product) =>
-    setProducts((prevProducts) =>
-      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)),
-    );
+    setProducts((prevProducts) => updateProductInList(prevProducts, updatedProduct));
 
   const addProduct = (newProduct: Product) =>
     setProducts((prevProducts) => [...prevProducts, newProduct]);
--- src/refactoring/hooks/utils/productUtils.ts
+++ src/refactoring/hooks/utils/productUtils.ts
@@ -5,3 +5,7 @@ export const findProductById = (products: Product[], productId: string) =>
 
 export const removeDiscountByIndex = (discounts: Discount[], index: number) =>
   discounts.filter((_, i) => i !== index);
+
+export const updateProductInList = (products: Product[], updatedProduct: Product): Product[] => {
+  return products.map((p) => (p.id === updatedProduct.id ? updatedProduct : p));
+};
